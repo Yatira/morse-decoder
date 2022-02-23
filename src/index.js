272c59391fe054c55fee02d487ff75e639c01a77
Yatira <irishovna@gmail.com>
2022-02-23 11:02:38 +0300

    feat: modify structure of the project

--- src/index.js
+++ src/index.js
@@ -37,54 +37,20 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(MORSE_TABLE) {
+function decode(expr) {
     // write your solution here
-
+    return expr
+    .split('   ')
+    .map(
+      elem => elem
+        .split(' ')
+        .map(
+          b => MORSE_TABLE[b]
+        ).join('')
+    ).join(' ');
+}
     
-        MORSE_TABLE = MORSE_TABLE.trim();
-        let refinedData = MORSE_TABLE.split('   ');
-        let result = [];
-
-        for (let i = 0; i < refinedData.length; i++) {
-          let temp = refinedData[i].split(' ');
-          for (let j = 0; j < temp.length; j++) {
-            if (MORSE_TABLE[temp[j]]) {
-              result.push(MORSE_TABLE[temp[j]]);
-            }
-          }
-
-          if (i !== refinedData.length - 1) {
-          result.push(' ');
-          }
-        }
-        return result.join('');
-      
-
+       
 module.exports = {
     decode
-}
-
-
-let result = "";
-  let arr = expr.split("");
-
-  for (let i = 0; i < expr.length / 10; i++) {
-    let letter = "";
-    let char = arr.slice(i * 10, (i + 1) * 10);
-    for (let x = 0; x < char.length / 2; x++) {
-      let dot = char.slice(x * 2, (x + 1) * 2).join("");
-      if (dot == "**") {
-        letter = " ";
-        break;
-      }
-      if (dot == "10") {
-        letter += ".";
-      }
-      if (dot == "11") {
-        letter += "-";
-      }
-    }
-    result += MORSE_TABLE[letter] ? MORSE_TABLE[letter] : " ";
-  }
-  return result;
 }
